56f665c6c03d48c793f7c53956640d5acac9a65f
HimbeerserverDE <himbeerserverde@gmail.com>
2022-01-03 20:00:50 +0100

    Change additional software (includes lightdm config)

--- install_additional.sh
+++ install_additional.sh
@@ -1,3 +1,7 @@
 #! /bin/bash
 # Install additional desktop programs
-sudo -E apt install sddm okular ark vlc gwenview firefox thunderbird
+sudo -E apt install lightdm atril vlc firefox thunderbird
+
+sudo cp -p i3.desktop /usr/share/xsessions/i3.desktop
+sudo cp -p i3-with-shmlog.desktop /usr/share/xsessions/i3-with-shmlog.desktop
+sudo cp -p lightdm.conf /etc/lightdm/lightdm.conf
